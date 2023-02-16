a50a6a6e467f28225fc3629f6840b6a75ddcdb15
levkovich-polina <levkovich.polina@gmail.com>
2023-02-16 19:18:44 +0300

    Made a program model and drawing squares

---
+++ Task12/Task12/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Task12
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel = new System.Windows.Forms.Panel();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BlueTextBox = new System.Windows.Forms.TextBox();
+            this.RedTextBox = new System.Windows.Forms.TextBox();
+            this.GreenTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel
+            // 
+            this.Panel.BackColor = System.Drawing.Color.White;
+            this.Panel.Location = new System.Drawing.Point(0, 1);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(620, 620);
+            this.Panel.TabIndex = 0;
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(679, 43);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(155, 56);
+            this.PlayButton.TabIndex = 1;
+            this.PlayButton.Text = "Начать игру";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BlueTextBox);
+            this.groupBox1.Controls.Add(this.RedTextBox);
+            this.groupBox1.Controls.Add(this.GreenTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(646, 157);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 235);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Поймано предметов";
+            // 
+            // BlueTextBox
+            // 
+            this.BlueTextBox.Location = new System.Drawing.Point(91, 155);
+            this.BlueTextBox.Name = "BlueTextBox";
+            this.BlueTextBox.Size = new System.Drawing.Size(90, 27);
+            this.BlueTextBox.TabIndex = 5;
+            // 
+            // RedTextBox
+            // 
+            this.RedTextBox.Location = new System.Drawing.Point(91, 109);
+            this.RedTextBox.Name = "RedTextBox";
+            this.RedTextBox.Size = new System.Drawing.Size(90, 27);
+            this.RedTextBox.TabIndex = 4;
+            // 
+            // GreenTextBox
+            // 
+            this.GreenTextBox.Location = new System.Drawing.Point(91, 65);
+            this.GreenTextBox.Name = "GreenTextBox";
+            this.GreenTextBox.Size = new System.Drawing.Size(90, 27);
+            this.GreenTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Синих";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Красных";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Зелёных";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 621);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.Panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel Panel;
+        private Button PlayButton;
+        private GroupBox groupBox1;
+        private TextBox BlueTextBox;
+        private TextBox RedTextBox;
+        private TextBox GreenTextBox;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ Task12/Task12/Form1.cs
@@ -0,0 +1,117 @@
+using Timer = System.Threading.Timer;
+
+
+namespace Task12
+{
+    public partial class Form1 : Form
+    {
+        public class Square
+        {
+            public int PositionX { get; set; }
+            public int PositionY { get; set; }
+
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public Color Color { get; set; }
+            public Square(int x, int y, int width, int height, Color color)
+            {
+                PositionX = x;
+                PositionY = y;
+                Width = width;
+                Height = height;
+                Color = color;
+            }
+        }
+
+        Timer _timer;
+        List<Square> _squares = new List<Square>();
+        List<int> _listPositionX = new List<int>();
+        Random _random = new Random();
+        int _positionX;
+        int _positionY;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            TimerCallback tm = new TimerCallback(OnTimerTicked);
+            _timer = new Timer(tm, 0, 0, 1000);
+        }
+
+        private void OnTimerTicked(object? state)
+        {
+            _listPositionX.Clear();
+            for (int j = 0; j < 31; j++)
+            {
+                int chance = _random.Next(1, 11);
+                if (chance <= 1)
+                {
+                    _listPositionX.Add(j);
+                }
+            }
+
+            int width = Panel.ClientSize.Width / 31;
+            int height = Panel.ClientSize.Height / 31;
+            _positionY = 0;
+            if (_squares.Count > 1)
+            {
+                for (int i = 0; i < _squares.Count; i++)
+                {
+                    _squares[i].PositionY += height;
+                }
+            }
+
+            for (int i = 0; i < _listPositionX.Count; i++)
+            {
+                _positionX = width * _listPositionX[i];
+                int randomNumberColor = _random.Next(0, 4);
+                if (randomNumberColor == 0)
+                {
+                    Square square = new Square(_positionX, _positionY, width, height, Color.Blue);
+                    _squares.Add(square);
+                }
+                else if (randomNumberColor == 1)
+                {
+                    Square square = new Square(_positionX, _positionY, width, height, Color.Red);
+                    _squares.Add(square);
+                }
+                else if (randomNumberColor == 2)
+                {
+                    Square square = new Square(_positionX, _positionY, width, height, Color.Green);
+                    _squares.Add(square);
+                }
+                else
+                {
+                    Square square = new Square(_positionX, _positionY, width, height, Color.Yellow);
+                    _squares.Add(square);
+                }
+            }
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Graphics g = Panel.CreateGraphics();
+            int height = Panel.ClientSize.Height / 31;
+
+            for (int i = 0; i < _squares.Count; i++)
+            {
+                var dx = _squares[i].PositionX;
+                var dy = _squares[i].PositionY;
+                var dWidth = _squares[i].Width;
+                var dHeight = _squares[i].Height;
+                var brush = _squares[i].Color;
+                Invoke(() =>
+                {
+                    g.FillRectangle(new SolidBrush(Color.White), dx, dy - height, dWidth, dHeight);
+
+                    g.FillRectangle(new SolidBrush(brush), dx, dy, dWidth, dHeight);
+                });
+            }
+        }
+    }
+}
